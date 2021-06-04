79248eeca681953a5f545a4fe2a3a17ee0ed3d26
mutonar <nazarov@vega-gaz.ru>
2021-06-04 11:35:41 +0300

    scope="prototype"

--- src/main/java/MinoroPackage/Main.java
+++ src/main/java/MinoroPackage/Main.java
@@ -25,15 +25,14 @@ public class Main {
         // обязательно файл applicationcontext.xml должен лежать в папке помеченной как Resource Root
         ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext("applicationcontext.xml");
         TestBean bean1 = context1.getBean("testBean", TestBean.class);
-        Animal cat = context1.getBean("animalBeanCat", Cat.class); // На прямую вызвать бин кота
-        AnalizeAnimal aAnimal = new AnalizeAnimal(cat);             // внедрение зависимости вручную
-        AnalizeAnimal aAnimal1 =  context1.getBean("AnalizeAnimalBean", AnalizeAnimal.class); // внедрение зависимости из контекта Бинов
-        AnalizeAnimal aAnimalSeter =  context1.getBean("AnalizeAnimalSeterBean", AnalizeAnimal.class); // внедрение зависимости из контекта Бинов
+        Animal cat = context1.getBean("animalBeanCat", Cat.class);  // На прямую вызвать бин кота
+        AnalizeAnimal aAnimal = new AnalizeAnimal(cat);                   // внедрение зависимости вручную
+        AnalizeAnimal aAnimal1 =  context1.getBean("AnalizeAnimalBean", AnalizeAnimal.class);           // внедрение зависимости из контекта Бинов
+        AnalizeAnimal aAnimalSeter =  context1.getBean("AnalizeAnimalSeterBean", AnalizeAnimal.class);  // внедрение зависимости из контекта Бинов
         //aAnimal1.getAnamalName();
         aAnimalSeter.getAnimalName();
         System.out.println("Name From Bean: " + aAnimalSeter.getAnimalName() +
                 " Weight: " + Integer.toString(aAnimalSeter.getWeight()));   // просто вывод
-
 
         // чтение и выдоид данных из файла
         AnalizeAnimal aAnimalSeterFromFile =  context1.getBean("AnalizeAnimalSeterBeanFromFile", AnalizeAnimal.class); // внедрение зависимости из контекта Бинов
@@ -41,6 +40,13 @@ public class Main {
                 " Weight: " + Integer.toString(aAnimalSeterFromFile.getWeight()));   // просто вывод
 
         aAnimalSeterFromFile.getListnameAnimal(); // Вывод листа который занесли с помощью xml конфига
+
+        //нескольно экземпляров бинов
+        Animal dog1 = context1.getBean("animalBeanDog", Dog.class);
+        Animal dog2 = context1.getBean("animalBeanDog", Dog.class);
+        dog1.setName("Betchovin");
+        System.out.println(dog1.getName());
+        System.out.println(dog2.getName());
 
         context1.close(); // закрываем работу с ним
 
